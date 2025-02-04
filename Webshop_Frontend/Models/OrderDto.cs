9783218d202240037991fd532c350167985c9c0e
stole25 <119949884+stole25@users.noreply.github.com>
2025-02-04 21:46:57 +0100

    Frontend uredivanje

--- Webshop_Frontend/Models/OrderDto.cs
+++ Webshop_Frontend/Models/OrderDto.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Webshop_Frontend.Models;
 
 public class OrderDto
 {
+    [Required(ErrorMessage = "Ime i prezime je obavezno")]
     public string FullName { get; set; }
+    
+    [Required(ErrorMessage = "Adresa je obavezna")]
     public string Address { get; set; }
+    
+    [Required(ErrorMessage = "Email je obavezan")]
+    [EmailAddress(ErrorMessage = "Neispravan format email adrese")]
     public string Email { get; set; }
-    public string Phone { get; set; }
+    
+    [Phone(ErrorMessage = "Neispravan format telefonskog broja")]
+    public string? Phone { get; set; }
+    
+    [Required(ErrorMessage = "Odaberite način plaćanja")]
     public PaymentMethod PaymentMethod { get; set; }
 }
